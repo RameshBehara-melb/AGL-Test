b81ab56d51045c1d37265434d54d1224242a47ad
Ramesh BEHARA <71306959+RameshBehara-melb@users.noreply.github.com>
2020-09-17 15:27:19 +1000

    Add files via upload

---
+++ AGLTest/AGLTest/src/CatLister/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CatLister.Services;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CatLister.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPersonService _personService;
+
+        //Would ordinarily set up IoC for this
+        public HomeController()
+        {
+            var _client = new TestableHttpClient(ConfigurationManager.AppSettings["DataSourceBaseUri"]);
+            _personService = new PersonService(_client);
+        }
+        public HomeController(IPersonService service)
+        {
+            _personService = service;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Title = "AGL Developer Test";
+
+            var model = await _personService.GetCatsByOwnerGenderAsync();
+            return View(model);
+        }
+    }
+}
